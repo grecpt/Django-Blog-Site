655c393616d56fcec9990cbb34732f36edcbf8a9
Rex Makusia <grecpt@gmail.com>
2024-08-27 23:53:34 +1000

    Django-Blog-Site: Update the application views

--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,16 @@ from django.core.paginator import Paginator,EmptyPage,\
                                             PageNotAnInteger
 def post_list(request):
     object_list = Post.published.all()
-    paginator   = Paginator(object_list,3) # 3 posts in  each page
+    paginator   = Paginator(object_list, 3) # 3 posts in  each page
     page        = request.GET.get('page')
     try:
-        posts    = paginator.page(page)
+        posts   = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer deliver the first page
-        posts    = paginator(1)
+        posts   = paginator.page(1)
     except EmptyPage:
         # If page is out of range deliver last page of results
-        posts    = paginator.page(paginator.num_pages)
+        posts   = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
