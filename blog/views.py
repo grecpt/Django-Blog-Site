c872856d9110e75f8504732bde66d6ab14e137a3
Rex Makusia <grecpt@gmail.com>
2024-08-27 22:13:43 +1000

    Django-Blog-Site: Update Application Templates

--- blog/views.py
+++ blog/views.py
@@ -13,9 +13,9 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,  slug=post,
                                     status='published',
-                                    published__year=year,
-                                    published__month=month,
-                                    published__day=day)
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
     return render(request, 
                 'blog/post/detail.html',
                 {'post' : post })
