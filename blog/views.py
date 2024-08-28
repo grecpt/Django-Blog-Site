d961a807e54c76f4fd6234590d93c6202a84f782
Rex Makusia <grecpt@gmail.com>
2024-08-28 19:37:28 +1000

    Django-Blog-Site:Application views updated

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator,EmptyPage,\
@@ -31,3 +32,9 @@ def post_detail(request, year, month, day, post):
     return render(request, 
                 'blog/post/detail.html',
                 {'post' : post })
+
+class PostListView(ListView):
+    queryset            = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by         = 3
+    template_name       = 'blog/post/list.html'
